8868260ef4a907ce34c5c09d040361dd8c7df072
Anargu <anthony.arostegui@gmail.com>
2021-05-19 19:54:35 -0500

    Initial Commit

---
+++ Daydream/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Daydream
+//
+//  Created by Anargu on 19/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
